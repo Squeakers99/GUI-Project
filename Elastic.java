39f1a9433ece52605b9a9926a6978e188faafb5d
Soheil Rajabali <soheilrajabali@gmail.com>
2024-04-30 11:48:00 -0400

    Calculations put into comments

--- Elastic.java
+++ Elastic.java
@@ -1,6 +1,30 @@
 import javax.swing.*;
 import java.awt.Dimension;
 
+/*
+ ? Relative Velocity Calculations to put the system in the frame of M2
+ if(dblV2Initial >= dblV1Initial) {
+    TODO: Pop up window saying collision will not happen
+ }else{
+    dblV1Initial = dblV1Initial - dblV2Initial
+ }
+
+ ? Elastic Collision Formulas assuming V2 is 0 
+ * dblV1Final = ( (dblM1 - dblM2) * dblV1Initial) / (dblM1 + dblM2)
+ * dblV2Final = ( 2 * dblM1 * dblV1Initial) / (dblM1+dblM2)
+ * 
+ 
+ ? Relative Colision Velocity Calculations to put Velocities back into their original frame
+ * dblV1Final = dblV1Final + dblV2Initial
+ * dblV2Final = dblV2Final + dblV2Initial
+ 
+ ? Momentum Calculations
+ * dblP1Initial = dblM1 * dblV1Initial
+ * dblP2Initial = dblM2 * dblV2Initial
+ * dblP1Final = dblM1 * dblV1Final
+ * dblP2Final = dblM2 * dblV2Final
+*/
+
 public class Elastic {
     JPanel theElasticPanel = new JPanel();
 
--- Inelastic.java
+++ Inelastic.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.event.*;
 
+/*
+ ? Inelastic calculations
+ * dblVFinal = (dblM1 * dblV1Initial + dblM2 * dblV2Initial) / (dblM1 + dblM2)
+ * dblP1Initial = dblM1 * dblV1Initial
+ * dblP2Initial = dblM2 * dblV2Initial
+ * dblPFinal = dblP1Initial + dblP2Initial
+ */
+
 public class Inelastic {
     JPanel InelasticPanel = new JPanel();
 
