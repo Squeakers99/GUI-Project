8b11a2708e796269590a4893441c097cd57dc82d
Soheil Rajabali <soheilrajabali@gmail.com>
2024-05-06 19:39:21 -0400

    Made the quiz file more efficient by using an array

--- Quiz.java
+++ Quiz.java
@@ -7,27 +7,37 @@ public class Quiz implements ActionListener{
 
     // Properties
     JPanel thePanel = new JPanel();
-    JLabel theAnswer1 = new JLabel("Answer 1");
-    JLabel theAnswer2 = new JLabel("Answer 2");
-    JLabel theAnswer3 = new JLabel("Answer 3");
-    JLabel theAnswer4 = new JLabel("Answer 4");
-    JLabel theAnswer5 = new JLabel("Answer 5");
-    JLabel theAnswer6 = new JLabel("Answer 6");
-    JLabel theAnswer7 = new JLabel("Answer 7");
-    JLabel theAnswer8 = new JLabel("Answer 8");
-    JLabel theAnswer9 = new JLabel("Answer 9");
-    JLabel theAnswer10 = new JLabel("Answer 10");
-    JLabel theAnswer11 = new JLabel("Answer 11");
-    JLabel theAnswer12 = new JLabel("Answer 12");
-    JLabel theAnswer13 = new JLabel("Answer 13");
-    JLabel theAnswer14 = new JLabel("Answer 14");
-    JLabel theAnswer15 = new JLabel("Answer 15");
+    JLabel[] theQuestion = new JLabel[15];
+    JRadioButton[] theAnswers = new JRadioButton[15];
+    int intQuestionNumber = 0;
 
     JScrollPane theScroll = new JScrollPane(thePanel);
 
     // Methods
+    @Override
     public void actionPerformed(ActionEvent evt){
 
+    }
+    
+    //Completes all the functionality to set up a question
+    public void setQuestion(String strQuestion, int intQuestionY){
+        //Sets up the question
+        theQuestion[intQuestionNumber] = new JLabel(strQuestion);
+        theQuestion[intQuestionNumber].setBounds(10, intQuestionY, 940, 30);
+        theQuestion[intQuestionNumber].setForeground(Color.white);
+        thePanel.add(theQuestion[intQuestionNumber]);
+
+        //Sets up the button
+        theAnswers[intQuestionNumber] = new JRadioButton();
+        theAnswers[intQuestionNumber].setBounds(10, intQuestionY, 20, 20);
+        theAnswers[intQuestionNumber].setForeground(Color.white);
+        thePanel.add(theAnswers[intQuestionNumber]);
+
+        //Adds the action listener
+        theAnswers[intQuestionNumber].addActionListener(this);
+
+        //Increments the question number
+        intQuestionNumber++;
     }
     //Constructor
     public Quiz(){
@@ -37,50 +47,22 @@ public class Quiz implements ActionListener{
         theScroll.setPreferredSize(new Dimension(960, 540));
         thePanel.setBackground(Color.black);
 
-        thePanel.add(theAnswer1);
-        theAnswer1.setBounds(10, 10, 200, 30);
-
-        thePanel.add(theAnswer2);
-        theAnswer2.setBounds(10, 60, 200, 30);
-
-        thePanel.add(theAnswer3);
-        theAnswer3.setBounds(10, 110, 200, 30);
-
-        thePanel.add(theAnswer4);
-        theAnswer4.setBounds(10, 160, 200, 30);
-
-        thePanel.add(theAnswer5);
-        theAnswer5.setBounds(10, 210, 200, 30);
-
-        thePanel.add(theAnswer6);
-        theAnswer6.setBounds(10, 260, 200, 30);
-
-        thePanel.add(theAnswer7);
-        theAnswer7.setBounds(10, 310, 200, 30);
-
-        thePanel.add(theAnswer8);
-        theAnswer8.setBounds(10, 360, 200, 30);
-
-        thePanel.add(theAnswer9);
-        theAnswer9.setBounds(10, 410, 200, 30);
-
-        thePanel.add(theAnswer10);
-        theAnswer10.setBounds(10, 460, 200, 30);
-
-        thePanel.add(theAnswer11);
-        theAnswer11.setBounds(10, 510, 200, 30);
-
-        thePanel.add(theAnswer12);
-        theAnswer12.setBounds(10, 560, 200, 30);
-
-        thePanel.add(theAnswer13);
-        theAnswer13.setBounds(10, 610, 200, 30);
-
-        thePanel.add(theAnswer14);
-        theAnswer14.setBounds(10, 660, 200, 30);
-
-        thePanel.add(theAnswer15);
-        theAnswer15.setBounds(10, 710, 200, 30);
+        //Sets up the questions
+        setQuestion("What is the capital of France?", 10);
+        setQuestion("What is the capital of Germany?", 40);
+        setQuestion("What is the capital of Italy?", 70);
+        setQuestion("What is the capital of Spain?", 100);
+        setQuestion("What is the capital of Portugal?", 130);
+        setQuestion("What is the capital of Belgium?", 160);
+        setQuestion("What is the capital of the Netherlands?", 190);
+        setQuestion("What is the capital of Luxembourg?", 220);
+        setQuestion("What is the capital of Switzerland?", 250);
+        setQuestion("What is the capital of Austria?", 280);
+        setQuestion("What is the capital of Poland?", 310);
+        setQuestion("What is the capital of the Czech Republic?", 340);
+        setQuestion("What is the capital of Slovakia?", 370);
+        setQuestion("What is the capital of Hungary?", 400);
+        setQuestion("What is the capital of Romania?", 430);
     }
     
 }
